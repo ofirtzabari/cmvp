c57873492885f3a0578751755cf83d9b6c3033ca
ofirtzabari <ofirtzabari@gmail.com>
2024-12-24 20:37:09 +0200

    fix bug

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -3,7 +3,7 @@ const path = require("path");
 const User = require("../model/user");
 const router = express.Router();
 const { upload } = require("../multer");
-const ErrorHandler = require("../utils/errorHandler");
+const ErrorHandler = require("../utils/ErrorHandler");
 const fs = require("fs");
 
 router.post("/create-user", upload.single("file"), async (req, res, next) => {
