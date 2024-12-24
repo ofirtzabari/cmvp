61b185335edc69502cc21ed70ef96822d2bb6aca
ofirtzabari <ofirtzabari@gmail.com>
2024-12-24 20:10:06 +0200

    fix bugs

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,30 +1,39 @@
-const express = require('express');
-const path = require('path');
-const User = require('../model/user');
+const express = require("express");
+const path = require("path");
+const User = require("../model/user");
 const router = express.Router();
-const {upload} = require('../multer');
-const ErrorHandler = require('../utils/errorHandler');
+const { upload } = require("../multer");
+const ErrorHandler = require("../utils/errorHandler");
 
-router.post("/create-user",upload.single("file"), async (req, res) => {
-    const {name, email, password} = req.body;
-    const userEmail = await User.findOne({email});
+router.post("/create-user", upload.single("file"), async (req, res, next) => {
+  const { name, email, password } = req.body;
+  console.log(name, email, password);
+  const userEmail = await User.findOne({ email });
 
-    if(userEmail){
-        return next(new ErrorHandler("User already exists",400));
-    }
+  if (userEmail) {
+    return next(new ErrorHandler("User already exists", 400));
+  }
+// need to fix this
+//   const fileName = req.file.filename;
+//   const fileURL = path.join(fileName);
+//   const avatar = fileURL;
 
-    const fileName = req.file.filename;
-    const fileURL = path.join(fileName);
-    const avatar = fileURL;
+  console.log("test");
+  const user = new User({
+    name,
+    email,
+    password
+  });
 
-    const user = new User({
-        name,
-        email,
-        password,
-        avatar: fileURL
-    });
-    console.log(user);
-    res.send("User created");
+  console.log(user);
+  const newUser = await User.create(user);
+
+  res.status(201).json({
+    success: true,
+    user: newUser,
+  });
+  
+  
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ connectDatabase();
 
 // Setting up config file
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
