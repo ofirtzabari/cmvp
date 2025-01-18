6e019d5cfc4ecddcf89881efbb0b00dce0d3bbfa
ofirtzabari <ofirtzabari@gmail.com>
2025-01-18 19:09:17 +0200

    add featured products

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Layout/Header.jsx'
 import Hero from '../components/Route/Hero/Hero.jsx'
 import Categories from '../components/Route/Categories/Categories.jsx'
 import BestDeals from '../components/Route/BestDeals/BestDeals.jsx'
+import FeaturedProduct from '../components/Route/FeaturedProduct/FeaturedProduct.jsx'
 
 function HomePage() {
   return (
@@ -11,6 +12,7 @@ function HomePage() {
         <Hero/>
         <Categories />
         <BestDeals/>
+        <FeaturedProduct/>
     </div>
   )
 }
